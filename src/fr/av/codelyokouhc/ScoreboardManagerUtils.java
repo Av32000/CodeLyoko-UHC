edb23c7d6a6f75e64f53d6fd43ca64c17f1a2a27
Av32000 <ae.av32000@gmail.com>
2022-07-31 10:44:55 +0200

    Fix Scoreboard

--- src/fr/av/codelyokouhc/ScoreboardManagerUtils.java
+++ src/fr/av/codelyokouhc/ScoreboardManagerUtils.java
@@ -9,25 +9,18 @@ import java.util.Map;
 public class ScoreboardManagerUtils {
     ScoreboardManager manager = Bukkit.getScoreboardManager();
     public void AddScoreBordToPlayer(Player player, Map<String, Integer> values) {
-        Scoreboard board = player.getScoreboard();
+        Scoreboard board = manager.getNewScoreboard();
         Objective objective = board.getObjective("Code Lyoko UHC");
-        if(objective == null){
-            objective = board.registerNewObjective("Code Lyoko UHC", "dummy");
-            objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-            objective.setDisplayName("§bCode Lyoko UHC");
+        if(objective != null) objective.unregister();
 
-            for (String key:values.keySet()) {
-                objective.getScore(key).setScore(values.get(key));
-            }
-        }else{
-            objective.unregister();
-            objective = board.registerNewObjective("Code Lyoko UHC", "dummy");
-            objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-            objective.setDisplayName("§bCode Lyoko UHC");
+        objective = board.registerNewObjective("Code Lyoko UHC", "dummy");
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+        objective.setDisplayName("§bCode Lyoko UHC");
 
-            for (String key:values.keySet()) {
-                objective.getScore(key).setScore(values.get(key));
-            }
+        for (String key:values.keySet()) {
+            objective.getScore(key).setScore(values.get(key));
         }
+
+        player.setScoreboard(board);
     }
 }
